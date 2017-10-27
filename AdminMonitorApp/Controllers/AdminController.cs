5841faa316326d6420d2abf42c7e5bb5abc2b29b
kkblues <kkkapasakis@yahoo.gr>
2017-10-27 22:21:02 +0300

    Delete soem files in admin project

--- AdminMonitorApp/Controllers/AdminController.cs
+++ AdminMonitorApp/Controllers/AdminController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace AdminMonitorApp.Controllers
 {
-    public class HomeController : Controller
+    public class AdminController : Controller
     {
         public ActionResult Index()
         {
