f3b250b96723931ca1e5cd30ce16b01d03fb2a83
Andreas <gesos.andreas@gmail.com>
2017-10-29 02:48:23 +0300

    Add ApplicationUserViewModel.

---
+++ WheelOfFortune/Models/ViewModels/ApplicationUserModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using WheelOfFortune.Models.Domain;
+
+namespace WheelOfFortune.Models.ViewModels
+{
+    public class ApplicationUserViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string UName { get; set; }
+        public string Email { get; set; }
+        public byte[] UserPhoto { get; set; }
+    }
+}
