9f50fcacfa84b95be4251714d5e601d70b4ab556
Andreas <gesos.andreas@gmail.com>
2017-10-29 01:09:57 +0300

    Update CouponBindingModel.

--- WheelOfFortune/Models/ViewModels/CouponModels.cs
+++ WheelOfFortune/Models/ViewModels/CouponModels.cs
@@ -5,8 +5,7 @@ namespace WheelOfFortune.Models.ViewModels
 {
     public class CouponBindingModel
     {
-        public string Code { get; set; }
-        public CouponValue Value { get; set; }
+        public int Value { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateExpired { get; set; }
         public bool Active { get; set; }
