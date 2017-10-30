132eab8e68806ce5c160cabe4bde6a0d36aa3635
Andreas <gesos.andreas@gmail.com>
2017-10-31 01:54:43 +0200

    Add balance models.

---
+++ WheelOfFortune/Models/ViewModels/BalanceModels.cs
@@ -0,0 +1,9 @@
+﻿namespace WheelOfFortune.Models.ViewModels
+{
+    public class BalanceViewModel
+    {
+        public int Id { get; set; }
+        public decimal BalanceValue { get; set; }
+        public ApplicationUserViewModel User { get; set; }
+    }
+}
