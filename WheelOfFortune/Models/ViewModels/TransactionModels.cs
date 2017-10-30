ce722fc9e4a772db09b4349599bcb4b77ebdc458
Andreas <gesos.andreas@gmail.com>
2017-10-30 02:49:25 +0200

    Add TransactionViewModel.

--- WheelOfFortune/Models/ViewModels/TransactionModels.cs
+++ WheelOfFortune/Models/ViewModels/TransactionModels.cs
@@ -10,4 +10,13 @@ namespace WheelOfFortune.Models.ViewModels
         public DateTime TransactionDate { get; set; }
         public ApplicationUser User { get; set; }
     }
+
+    public class TransactionViewModel
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+        public TransactionType Type { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public ApplicationUserViewModel User { get; set; }
+    }
 }
